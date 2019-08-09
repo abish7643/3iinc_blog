06262cfd8867d3f7f9aa550bfc6bd08f8d0127c0
abish7643 <abishvijayan7643@gmail.com>
2019-08-09 10:26:23 +0530

    Password Reset NW

---
+++ .bash_profile
@@ -0,0 +1,2 @@
+export EMAIL_USER="abishvijayan7643@gmail.com"
+export EMAIL_PASS="reset123@!"
--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,12 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from PIL import Image
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=140, default='SOME STRING')
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
